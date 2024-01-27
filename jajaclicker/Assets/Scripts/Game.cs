219922c34f33b93506ee780151f231f5eeb4c40a
Valeria Colombo <valeriaccolombo@gmail.com>
2024-01-27 20:52:49 -0300

    done

---
+++ jajaclicker/Assets/Scripts/Game.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public AudioClip[] jas;
+    public Sprite[] big;
+    public Sprite[] small;
+    public Image buttonBig;
+    public Image buttonSmall;
+    public Animator anim;
+    public AudioSource audioSouce;
+    public TMP_Text timesTxt;
+
+    private int currentColor;
+    private int times = 0;
+
+    void Start()
+    {
+        ChangeColor();
+        timesTxt.text = times.ToString();
+    }
+
+    private void ChangeColor()
+    {
+        currentColor = Random.Range(0, big.Length);
+
+        buttonBig.sprite = big[currentColor];
+        buttonSmall.sprite = small[currentColor];
+    }
+
+    public void OnClick() 
+    {
+        anim.Play("click");
+        ChangeColor();
+
+        times++;
+        timesTxt.text = times.ToString();
+
+        audioSouce.clip = jas[Random.Range(0,jas.Length)];
+        audioSouce.loop = false;
+        audioSouce.Play();
+    }
+
+}
